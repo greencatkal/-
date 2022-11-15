016048c5aa56377c18918e61273fdd73c655d31e
greencatkal <2364785195@qq,com>
2022-11-15 19:45:24 +0800

    感觉思路不太清楚

--- 图书馆信息管理系统.cpp
+++ 图书馆信息管理系统.cpp
@@ -1,8 +1,26 @@
 #include<iostream>
 using namespace std;
 
+void IdenTity(int n)
+{
+
+}
+
 int main(){
     
+    cout<<"你的身份是"<<'\n';
+    cout<<"1.管理员"<<'\n';
+    cout<<"2.学生"<<'\n';
+    cout<<"0.退出图书馆信息管理系统"<<'\n';
+    
+    int Choice;
+    cin>>Choice;
+
+    if(Choice==0)
+    return 0;
+
+    IdenTity(Choice);
+
     system("pause");
     return 0;
 }
