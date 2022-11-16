8bfd8a6892c77be417615e165b92b45278426d73
MRWXUSP <2196868714@qq.com>
2022-11-16 22:27:04 +0800

    11-16更新

--- hnulib.h
+++ hnulib.h
@@ -2,11 +2,15 @@
 # define _HUNLIB_H
 
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 class books{    //图书信息
+friend struct books* loading_books(int &num);
+friend struct admin* loading_admin(int &num);
 friend user;
 friend admin;
+friend menu;
 public:
     bool borrow;    //该图书是否被借阅
     void lookup_name(wstring name);  //书名查找图书
@@ -18,20 +22,25 @@ public:
     void new_publish(); //最新出版排行榜
 
 protected:
+    long int num;   //图书数目
     int id; //图书id
     string isbn;    //图书isdn，较长
-    wstring name;   //图书名，有中文
-    wstring author; //作者名
-    wstring publishing; //出版社名字
+    string name;   //图书名，有中文
+    string author; //作者名
+    string publishing; //出版社名字
     string published;   //出版日期
     double price;   //图书价格，含小数
     int pages;  //图书页数
-    wstring description;    //图书描述
+    string description;    //图书描述
+    struct books *next;
 
 };
 
 class user{ //读者（用户）
+friend struct books* loading_books(int &num);
+friend struct admin* loading_admin(int &num);
 friend admin;
+friend menu;
 public:
     void change_key();  //更改密码
     void show_borrow(); //查看借阅记录
@@ -40,11 +49,13 @@ protected:
     long int account_num;   //账号
     long int key;   //密码
     int log[20];    //借阅记录，最多借阅20本书
+    user *next;
 
 };
 
 class admin{    //管理员
-
+friend struct books* loading_books(int &num);
+friend struct admin* loading_admin(int &num);
 public:
     void add_book();    //增加图书
     void del_book();    //删除图书
@@ -53,11 +64,11 @@ public:
 protected:
     long int account_num;
     long int key;
+    admin *next;
 
 };
 
-class menus{
-
+class menu{
     void show_menu_main();  //主目录
     void show_menu_admin(); //管理员目录
     void show_menu_user();  //使用者目录
@@ -66,5 +77,8 @@ class tourist{  //游客（待开发）
 
 
 };
+
+struct books* loading_books(int &num);
+struct admin* loading_admin(int &num);
 
 # endif
