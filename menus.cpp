99c7c33b38b3f693d2609b3174e1e68a1e4e2f20
greencatkal <2364785195@qq,com>
2022-11-16 22:48:05 +0800

    tried

---
+++ menus.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include "hnulib.h"
+using namespace std;
+
+void menus::show_menu_main()
+{
+    cout<<"你的身份是"<<'\n';
+    cout<<"1.管理员"<<'\n';
+    cout<<"2.学生"<<'\n';
+    cout<<"0.退出图书馆信息管理系统"<<'\n';
+}
--- 图书馆信息管理系统.cpp
+++ 图书馆信息管理系统.cpp
@@ -3,27 +3,29 @@
 
 using namespace std;
 
+menus show;
+
 void IdenTity(int n)    //感觉没那么对，到时候用不了就删
 {
+   
     switch (n)
     {
     case 1:
         //怎么调用都很奇怪，给用户操作部分再来一个函数吧
+        show.show_menu_admin();
         break;
     case 2:
-
+        show.show_menu_user();
         break;
     default:
+        cout<<"输入不规范"<<'\n';
         break;
     }
 }
 
 int main(){
     
-    cout<<"你的身份是"<<'\n';
-    cout<<"1.管理员"<<'\n';
-    cout<<"2.学生"<<'\n';
-    cout<<"0.退出图书馆信息管理系统"<<'\n';
+    show.show_menu_main();
     
     int Choice;
     cin>>Choice;
