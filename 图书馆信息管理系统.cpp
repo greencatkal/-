7c1fc9e615ba07511deace8631af3c8bb78de7a0
greencatkal <2364785195@qq,com>
2022-11-26 13:43:59 +0800

    ‘啊

--- 图书馆信息管理系统.cpp
+++ 图书馆信息管理系统.cpp
@@ -7,15 +7,16 @@ menu show;
 
 int main()
 { 
+    
+    int book_num,ad_num,us_num;
+
+    books* book_head=loading_books(book_num);
+    admin* admin_head=loading_admin(ad_num);
+    user* user_head=loading_users(us_num,book_head);
+
     while (true)
     {
         int i;
-
-        int book_num,ad_num,us_num;
-
-        books* book_head=loading_books(book_num);
-        admin* admin_head=loading_admin(ad_num);
-        user* user_head=loading_users(us_num,book_head);
 
         cout<<"欢迎使用图书馆信息管理系统！"<<'\n';
         i=show.show_menu_main();
